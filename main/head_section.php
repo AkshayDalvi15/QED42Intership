840a738e551d4f0799bb7dea60f4ec0b8b7a10fb
AkshayDalvi15 <88661890+AkshayDalvi15@users.noreply.github.com>
2021-09-29 21:27:04 +0530

    #8 commiting css files for main blog pages.

--- main/head_section.php
+++ main/head_section.php
@@ -9,5 +9,5 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<!-- Styling for public area -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-	<link rel="stylesheet" href="style/style.css">
+	<link rel="stylesheet" href="style/css/main.css">
 	<meta charset="UTF-8">
